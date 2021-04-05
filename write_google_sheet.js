6619caa6dcc4b6251382e9b32571227ba03c52d6
gunjan-k <42995365+gunjan-k@users.noreply.github.com>
2021-04-05 17:50:30 +0530

    Update write_google_sheet.js

--- write_google_sheet.js
+++ write_google_sheet.js
@@ -135,7 +135,7 @@ async function writeData(auth) {
 
 // Chedcking for the sprint - 
     
- let sprint_data = await jira.sprint.getSprintIssues( 
+  let sprint_data = await jira.getSprintIssues( 
      {
          rapidViewId: "8"
      }
