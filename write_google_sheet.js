57bf0dffc4decf4c2824d57f9b0fbf0c54ec87db
gunjan-k <42995365+gunjan-k@users.noreply.github.com>
2021-04-05 23:27:32 +0530

    Update write_google_sheet.js

--- write_google_sheet.js
+++ write_google_sheet.js
@@ -125,7 +125,7 @@ async function writeData(auth) {
     //Extracting the field from JIRA ticket
 
 
-   let result = await jira.issue.getIssue(
+   let result = await jira.getIssue(
         {
             //issueKey: "NX-2301" }, function(error, issue) {
             //issueKey: "NX-2301"
@@ -135,12 +135,12 @@ async function writeData(auth) {
 
 // Chedcking for the sprint - 
     
-  let sprdata = await jira.issue.getSprintIssues( 
+ /* let sprdata = await jira.issue.getSprintIssues( 
      {
          rapidViewId: "8"
      }
  );
-
+*/
     /* let values = [
         [
             'Chris',
@@ -170,7 +170,7 @@ async function writeData(auth) {
     console.log(result);
     console.log(result.key);
     console.log(result.fields.statuscategorychangedate);
-    console.log(sprdata);
+    //console.log(sprdata);
 
     sheets.spreadsheets.values.append({
         spreadsheetId: '1J_B6QUZmg3Zjqi2sw79w4hfYIHwJ7R8oq-ExUPq8Flo',
